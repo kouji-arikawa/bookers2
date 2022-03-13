8c1300419f1365fd3ec48cce61ac57a61c579e52
EC2 Default User <ec2-user@ip-172-31-93-104.ec2.internal>
2022-03-13 11:23:55 +0000

    file_setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_094040) do
+ActiveRecord::Schema.define(version: 2022_03_12_165332) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
